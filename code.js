445fa9fb5bb982f25e32116c181d6a81542f52cb
Nolan Berg <nolanb912@gmail.com>
2024-05-08 21:04:37 -0600

    update code.js and code.test.js

--- code.js
+++ code.js
@@ -1,9 +1,11 @@
-function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
-}
-
 function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    let result = 1.0;
+    let factorial = 1.0;
+
+    for (let i = 1; i <= n; i++) {
+        factorial *= i;
+        result += 1.0 / factorial;
+    }
+
+    return result;
 }
